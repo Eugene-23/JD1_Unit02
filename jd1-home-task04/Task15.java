a106907e41102fbcc79227cc57159f2bfb0ad8ba
Eugene-23 <54112436+Eugene-23@users.noreply.github.com>
2019-09-15 19:54:28 +0300

    Update Task15.java

--- jd1-home-task04/Task15.java
+++ jd1-home-task04/Task15.java
@@ -9,11 +9,43 @@ public class Task15 {
 
 	public static void main(String[] args) {
 		
-		int k = 458;
-		int n = 761;
-		
-		qqq
+		int[] mas = new int[3];
+		int k = 23;
+		int n = 8;
 
+		findNum(mas, k, n);
+
+		printMas(mas);
+
+	}
+
+	public static void findNum(int[] mas, int k, int n) {
+
+		int a;
+		int b;
+		int c;
+		int d;
+
+		for (int i = 100; i < 999; i++) {
+			a = i % 10;
+			b = (i / 10) % 10;
+			c = (i / 100) % 10;
+			d = a + b + c;
+
+			if ((d == k) && (a <= n) && (b <= n) && (c <= n)) {
+				mas[0] = a;
+				mas[1] = b;
+				mas[2] = c;
+
+			}
+		}
+
+	}
+	
+	public static void printMas(int[] mas) {
+		for(int i = 0; i < mas.length; i++) {
+			System.out.println(mas[i]);
+		}
 	}
 
 }
